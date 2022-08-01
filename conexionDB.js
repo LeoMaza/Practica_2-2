c2f4c03a2588779b198d843f12daceaf0b8a7aac
Leo Maza <leomaza2222@gmail.com>
2022-08-01 18:57:08 -0300

    Actualizacion del archivo implementando las operaciones CRUD

--- conexionDB.js
+++ conexionDB.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql"); //pido usar el modulo mysql "conector"
 require("dotenv").config();
+let sqlComando = "DELETE FROM platos WHERE id_plato = 2";
 
 //crear la conexion con los datos protegidos con la constantes utilizadas en el archivo .env
 const conexionDB = mysql.createConnection({
@@ -23,6 +24,31 @@ conexionDB.query("SELECT * FROM platos", (error, registro) => {
     }
     console.log("mostrando registro en la tabla \n", registro)
 })
+/*  //insertando nuevo registro
+conexionDB.query("INSERT INTO platos(nombre, precio, con_oferta)VALUES('ensalada cesar', 800, FALSE)", (error, resultado) => {
+    if (error) {
+        throw error;
+    }
+    console.log("Insertando nuevo registro \n", resultado)
+})*/
+
+
+/*//Actualizando nuevo registro
+conexionDB.query("UPDATE platos SET con_oferta = FALSE WHERE con_oferta = TRUE", (error, resultado) => {
+    if (error) {
+        throw error;
+    }
+    console.log("Actualizamos un registro en la tabla \n", resultado)
+})*/
+
+/*//Eliminar un registro
+conexionDB.query(sqlComando, (error, resultado) => {
+    if (error) {
+        throw error;
+    }
+    console.log("Eliminamos un registro  \n", resultado)
+})*/
+
 // cierra la conexion con la base de datos
 conexionDB.end((error) => {// una funcion sin nombre puede usarse de esta se le conocw como arrow function 
     if (error) {
